dee4b2816bbd7227dc4195b3b27bb37b7932ca86
TurtlePU <sokolov.p64@gmail.com>
2025-07-06 22:36:08 +0300

    + Traversable trees

--- src/Hermeneutics/Tree.hs
+++ src/Hermeneutics/Tree.hs
@@ -10,6 +10,8 @@ import Control.Category ((<<<))
 import Data.Bifunctor (Bifunctor, bimap)
 import Data.Bifoldable (Bifoldable, bifoldMap)
 import Hermeneutics.GFoldable (GFoldable, gfoldMap)
+import Data.Bitraversable (Bitraversable, bitraverse)
+import Hermeneutics.GTraversable (GTraversable, gtraverse)
 
 --------------------------------------------------------------------------------
 
@@ -35,6 +37,10 @@ instance Foldable (t (Tree t)) => Foldable (Tree t) where
     foldMap f (Leaf a) = f a
     foldMap f (Node n) = foldMap f n
 
+instance (Tensor t, Traversable (t (Tree t))) => Traversable (Tree t) where
+    traverse f (Leaf a) = fmap Leaf (f a)
+    traverse f (Node n) = fmap Node (traverse f n)
+
 --------------------------------------------------------------------------------
 
 instance Functor f => Tensor ((:.:) f) where
@@ -46,11 +52,20 @@ type Free f = Tree ((:.:) f)
 
 newtype BiScoped b m a = MkBiScoped { runBiScoped :: b (m (Maybe a)) (m a) }
 
+instance (Bifunctor b, Functor f) => Functor (BiScoped b f) where
+    fmap f = MkBiScoped . bimap (fmap f <$>) (fmap f) . runBiScoped
+
 instance Bifunctor b => Tensor (BiScoped b) where
     MkBiScoped b >>>= f = MkBiScoped $ bimap (>>= traverse f) (>>= f) b
 
 instance (Bifoldable b, Foldable f) => Foldable (BiScoped b f) where
     foldMap f = bifoldMap (foldMap (foldMap f)) (foldMap f) . runBiScoped
+
+instance (Bitraversable b, Traversable f) => Traversable (BiScoped b f) where
+    traverse f =
+        fmap MkBiScoped
+        . bitraverse (traverse (traverse f)) (traverse f)
+        . runBiScoped
 
 type Foil b = Tree (BiScoped b)
 
@@ -64,11 +79,25 @@ newtype Scope a n = MkScope { runScope :: Either (Fin n) a }
 
 newtype Scoped g m a = MkScoped { runScoped :: g (m :.: Scope a) }
 
+instance (Grammar g, Functor f) => Functor (Scoped g f) where
+    fmap f =
+        MkScoped
+        . gmap (Comp1 . fmap (MkScope . fmap f . runScope) . unComp1)
+        . runScoped
+
 instance Grammar g => Tensor (Scoped g) where
     MkScoped g >>>= f = MkScoped $
         gmap (Comp1 <<< fmap MkScope . traverse f . runScope <=< unComp1) g
 
 instance (GFoldable g, Foldable f) => Foldable (Scoped g f) where
     foldMap f = gfoldMap (foldMap (foldMap f . runScope) . unComp1) . runScoped
+
+instance (GTraversable g, Traversable f) => Traversable (Scoped g f) where
+    traverse f =
+        fmap MkScoped
+        . gtraverse (fmap Comp1
+                     . traverse (fmap MkScope . traverse f . runScope)
+                     . unComp1)
+        . runScoped
 
 type Term g = Tree (Scoped g)
