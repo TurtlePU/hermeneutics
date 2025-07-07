f6963afef316a42e6a9fc365b96a455303dbeaa0
TurtlePU <sokolov.p64@gmail.com>
2025-07-07 12:46:54 +0300

    remove examples

--- src/Hermeneutics/Multisorted.hs
+++ src/Hermeneutics/Multisorted.hs
@@ -5,13 +5,11 @@ module Hermeneutics.Multisorted where
 
 import Data.List.NonEmpty (NonEmpty (..))
 import Hermeneutics.Grammar (Grammar (..))
-import GHC.Generics ((:+:) (..), V1)
+import GHC.Generics ((:+:) (..))
 
 --------------------------------------------------------------------------------
 
-data Tree t g v s where
-    Leaf :: v s -> Tree t g v s
-    Node :: g s (t (Tree t g) v) -> Tree t g v s
+data Tree t g v s = Leaf (v s) | Node (g s (t (Tree t g) v))
 
 type f ~> g = forall a. f a -> g a
 
@@ -25,7 +23,7 @@ m =>> f = bind1 f m
 class Tensor1 t where
     lift1 :: Monad1 m => (a ~> m b) -> (t m a ~> t m b)
 
-instance (Tensor1 t, forall ss. Grammar (g ss)) => Monad1 (Tree t g) where
+instance (Tensor1 t, forall s. Grammar (g s)) => Monad1 (Tree t g) where
     unit1 = Leaf
     bind1 f (Leaf u) = f u
     bind1 f (Node n) = Node (gmap (lift1 f) n)
@@ -39,20 +37,6 @@ instance Tensor1 Id2 where
 
 -- | Multisorted grammars without bindings
 type FreeS = Tree Id2
-
-data AlgSort = Const | Algebra
-
-data Example s v where
-    (:.*:) :: v Const -> v Algebra -> Example Algebra v
-    (:**:) :: v s -> v s -> Example s v
-    (:++:) :: v s -> v s -> Example s v
-
-instance Grammar (Example s) where
-    gmap f (c :.*: a) = f c :.*: f a
-    gmap f (a :**: b) = f a :**: f b
-    gmap f (a :++: b) = f a :++: f b
-
-type AlgTerms = FreeS Example
 
 --------------------------------------------------------------------------------
 
@@ -70,28 +54,3 @@ instance Tensor1 Ext where
 
 -- | Multisorted grammars with bindings
 type Scoped = Tree Ext
-
-data Sort = Term | Type
-
-data Lam s v where
-    Un :: Lam Type v
-    Ar :: v (Type :| '[]) -> v (Type :| '[]) -> Lam Type v
-
-    S :: Lam Term v
-    Ap :: v (Term :| '[]) -> v (Term :| '[]) -> Lam Term v
-    Ab :: v (Type :| '[]) -> v (Term :| '[Term]) -> Lam Term v
-
-instance Grammar (Lam s) where
-    gmap _ Un = Un
-    gmap f (Ar t u) = Ar (f t) (f u)
-    gmap _ S = S
-    gmap f (Ap t u) = Ap (f t) (f u)
-    gmap f (Ab t b) = Ab (f t) (f b)
-
-type STLC = Scoped Lam
-
-data OnlyTerm s where OT :: OnlyTerm Term
-
--- an example of well-scoped lambda term in empty context.
-closed :: STLC V1 Term
-closed = Leaf OT =>> \OT -> Node (MkExt (Node S) `Ap` MkExt (Node S))
