a8a58cbb712154579706ee36a1a6f9367c9c6032
TurtlePU <sokolov.p64@gmail.com>
2025-07-16 21:28:59 +0300

    some docs

--- src/Hermeneutics/Flavours/ManySorted.hs
+++ src/Hermeneutics/Flavours/ManySorted.hs
@@ -1,10 +1,25 @@
+-- |
+-- Module      : Hermeneutics.Flavours.ManySorted
+-- Description : Adapter for working with many-sorted algebraic grammars.
+-- Copyright   : (c) TurtlePU, 2025
+-- License     : BSD-3
+-- Maintainer  : sokolov.p64@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This module contains an adapter to lift a non-scoped many-sorted grammar
+-- (also called "algebraic" here) into scoped many-sorted grammar.
 module Hermeneutics.Flavours.ManySorted where
 
 import Data.List.NonEmpty (NonEmpty (..))
 import Hermeneutics.Flavours (HFoldable (..), HFunctor (..), HTraversable (..))
 
+-- | @'SApp' v@ lowers @v@ from general term provider
+-- to the one used in algebraic grammars.
 newtype SApp v s = MkSApp { runSApp :: v (s :| '[]) }
 
+-- | Given an algebraic grammar @g@, @'ManySorted' g@ is a scoped grammar
+-- defining the same language.
 newtype ManySorted g v s = MSorted { runMSorted :: g (SApp v) s }
 
 instance HFunctor g => HFunctor (ManySorted g) where
--- src/Hermeneutics/Flavours/SecondOrder.hs
+++ src/Hermeneutics/Flavours/SecondOrder.hs
@@ -1,3 +1,16 @@
+-- |
+-- Module      : Hermeneutics.Flavours.SecondOrder
+-- Description : Adapter for working with scoped single-sorted grammars
+--               defined via a 'Bifunctor'.
+-- Copyright   : (c) TurtlePU, 2025
+-- License     : BSD-3
+-- Maintainer  : sokolov.p64@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This module contains an adapter 'SecondOrder' which turns
+-- a scoped single-sorted grammar defined via a 'Bifunctor'
+-- into a scoped multi-sorted 'HFunctor' that generates the same language.
 module Hermeneutics.Flavours.SecondOrder where
 
 import Data.Bifoldable (Bifoldable, bifoldMap)
@@ -6,6 +19,12 @@ import Data.Bitraversable (Bitraversable, bitraverse)
 import Data.List.NonEmpty (NonEmpty (..))
 import Hermeneutics.Flavours (HFoldable (..), HFunctor (..), HTraversable (..))
 
+-- | Given a 'Bifunctor' @b@, a @'SecondOrder' b@ is an 'HFunctor' generating
+-- the same language.
+--
+-- In order to derive instances of 'Bifunctor'-like classes, you can use
+-- Template Haskell functions provided in the
+-- [bifunctors](https://hackage.haskell.org/package/bifunctors) package.
 newtype SecondOrder b v s =
     SndOrder { sndOrder :: b (v (s :| '[s])) (v (s :| '[])) }
 
