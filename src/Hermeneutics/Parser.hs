265251d17c642a037228e0d6056767a31a274f1d
TurtlePU <sokolov.p64@gmail.com>
2026-04-01 11:24:58 +0300

    styling fix

--- src/Hermeneutics/Parser.hs
+++ src/Hermeneutics/Parser.hs
@@ -1,6 +1,6 @@
 module Hermeneutics.Parser where
 
-import GHC.TypeLits (Symbol)
 import Data.Kind (Type)
+import GHC.TypeLits (Symbol)
 
 newtype (s :: Type) :~ (r :: Symbol) = Lexeme { sourceText :: s }
